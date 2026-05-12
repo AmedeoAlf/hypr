162bf35721a782583e314947ce50bb653593867b
AmedeoAlf <amedeo.alfonsi@yahoo.com>
2026-05-12 20:59:10 +0200

    Restore wallpaper on laptop, rebind exit shortcut

--- amedeo-lenovo-v15.lua
+++ amedeo-lenovo-v15.lua
@@ -4,10 +4,16 @@ hl.monitor({
 	scale = "1.25",
 })
 
+---@param combo string
+---@param dispatcher HL.Dispatcher|function
+local function rebind(combo, dispatcher)
+	hl.unbind(combo)
+	hl.bind(combo, dispatcher)
+end
+
 -- TODO: more graceful logout
-hl.bind("SUPER + apostrophe", function()
+rebind("SUPER + apostrophe", function()
 	hl.exec_cmd("uwsm stop")
 end)
 
-hl.unbind("SUPER + B")
-hl.bind("SUPER + B", hl.dsp.exec_cmd("helium-browser"))
+rebind("SUPER + B", hl.dsp.exec_cmd("helium-browser"))
