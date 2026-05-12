49e5c72c1e307cda9b2ffb46c875e4dfc370068a
AmedeoAlf <amedeo.alfonsi@yahoo.com>
2026-05-12 16:09:04 +0200

    activation env for waybar (i think)

--- config/autostart.lua
+++ config/autostart.lua
@@ -1,11 +1,11 @@
 local programs = {
 	"hyprlauncher -d",
-	"waybar",
 	"udiskie",
 	"/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1",
 	"hyprpaper",
 	"hyprsunset",
 	"copyq --start-server",
+	"dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP",
 }
 
 hl.on("hyprland.start", function()
