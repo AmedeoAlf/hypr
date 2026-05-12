813ee14e1dcca50227c083655fb69fead723ff81
amedeo <amedeo.alfonsi@yahoo.com>
2026-05-12 14:52:23 +0200

    add hk to immediate mode

--- config/window_rules.lua
+++ config/window_rules.lua
@@ -17,4 +17,14 @@ local floating = {
 for _, title in pairs(floating) do
 	table.insert(M, { match = { initial_title = "^(" .. title .. ")$" }, float = true })
 end
+
+local immediate = {
+	"^Minecraft.*$",
+	"^(Hollow Knight)$",
+}
+
+for _, title in pairs(immediate) do
+	table.insert(M, { match = { title = title }, immediate = true })
+end
+
 return M
