c9ecc43b5c10fd3c971c348a8cc18cd5814d0511
amedeo <amedeo.alfonsi@yahoo.com>
2026-05-12 15:17:35 +0200

    add default.lua

---
+++ config/default.lua
@@ -0,0 +1,28 @@
+hl.config({
+	input = {
+		kb_layout = "it,us",
+		kb_options = "grp:shift_caps_toggle",
+
+		numlock_by_default = true,
+
+		follow_mouse = 1,
+		sensitivity = -0.4,
+		accel_profile = "flat",
+	},
+	general = {
+		-- Please see https://wiki.hyprland.org/Configuring/Tearing/ before you turn this on
+		allow_tearing = true,
+	},
+	render = {
+		ctm_animation = 1,
+	},
+	misc = {
+		vrr = 1,
+		mouse_move_enables_dpms = true,
+		key_press_enables_dpms = true,
+		middle_click_paste = false,
+	},
+	cursor = {
+		zoom_rigid = false,
+	},
+})
