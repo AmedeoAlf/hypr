a1bfdd0e7bfe8583bcfd80268c32e304804bea65
amedeo <amedeo.alfonsi@yahoo.com>
2026-05-12 14:53:10 +0200

    Reorder windowrules

--- config/window_rules.lua
+++ config/window_rules.lua
@@ -1,6 +1,5 @@
 ---@type HL.WindowRuleSpec[]
 local M = {
-	{ match = { initial_title = "^Minecraft.*$" }, immediate = true },
 	{
 		match = { initial_title = "^packettracer6.exe$" },
 		suppress_event = "fullscreen maximize activate activatefocus",
@@ -14,14 +13,14 @@ local floating = {
 	"org.kde.kcalc",
 }
 
-for _, title in pairs(floating) do
-	table.insert(M, { match = { initial_title = "^(" .. title .. ")$" }, float = true })
-end
-
 local immediate = {
 	"^Minecraft.*$",
 	"^(Hollow Knight)$",
 }
+
+for _, title in pairs(floating) do
+	table.insert(M, { match = { initial_title = "^(" .. title .. ")$" }, float = true })
+end
 
 for _, title in pairs(immediate) do
 	table.insert(M, { match = { title = title }, immediate = true })
